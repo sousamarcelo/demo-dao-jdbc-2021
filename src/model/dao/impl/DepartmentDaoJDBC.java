13bfc16ddf36c0483f4b3ddd72e595a3fb95cf26
sousamarcelo <marcelo.sousa@aluno.faculdadeimpacta.com.br>
2021-08-01 11:45:32 -0300

    findById department implementation - (exercise)

--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -1,8 +1,13 @@
 package model.dao.impl;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import model.dao.DepartmentDao;
 import model.entities.Department;
 
@@ -34,8 +39,39 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
 	@Override
 	public Department findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement pst = null;
+		ResultSet rs = null;
+		
+		try {
+			
+			pst = conn.prepareStatement(
+					"SELECT * FROM coursejdbc.department "
+					+ "where Id = ?"
+					);
+			pst.setInt(1, id);
+			
+			rs = pst.executeQuery();
+			if (rs.next()) {
+				Department dep = instanteateDepartment(rs);
+				return dep;
+			}
+			
+			return null;
+			
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(pst);
+			DB.closeResultSet(rs);
+		}		
+		
+	}
+
+	private Department instanteateDepartment(ResultSet rs) throws SQLException {
+		Department dep = new Department();
+		dep.setId(rs.getInt("Id"));
+		dep.setName(rs.getString("Name"));
+		return dep;
 	}
 
 	@Override
